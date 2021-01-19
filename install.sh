0da1eef99a01627254d4642da684bf96c13ff718
0i0 <liorhakim@gmail.com>
2021-01-19 08:17:17 +0200

    oops

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 brew install --cask alacritty
 
+# clone
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+
 # Install terminfo globally, I'm thinking this is to make the awesome
 # true colors & italic fonts work
 # We also change default terminal to alacritty in ~/.tmux.conf to use this
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+cd ..
+rm -rf alacritty
 
 # Enable smoothing on mac
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
